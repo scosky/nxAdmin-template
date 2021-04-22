7bac801eec5023ab677f9610bda0d818d59b2b40
sunhy <sunhaiyang@baiqiyifu.onaliyun.com>
2021-04-23 02:09:36 +0800

    #fea http 请求超时时间

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,57 +5,57 @@ import { baseUrl } from '@/config/env'
 import { getToken } from '@/utils/auth'
 // 创建axios实例
 const service = axios.create({
-  baseURL: baseUrl,
-  timeout: 5000
+    baseURL: baseUrl,
+    timeout: 10000
 })
 
 // request拦截器
 service.interceptors.request.use(config => {
-  if (store.getters.token) {
-    config.headers.common['token'] = getToken()
-  }
-  config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-  config.withCredentials = true
-  return config
+    if (store.getters.token) {
+        config.headers.common['token'] = getToken()
+    }
+    config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    config.withCredentials = true
+    return config
 }, error => {
-  console.log(error)
-  Promise.reject(error)
+    console.log(error)
+    Promise.reject(error)
 })
 
 service.interceptors.response.use(
-  response => {
-    const res = response.data
-    if (res.status === 1) {
-      Message({
-        message: res.msg,
-        type: 'error',
-        duration: 3 * 1000
-      })
+    response => {
+        const res = response.data
+        if (res.status === 1) {
+            Message({
+                message: res.msg,
+                type: 'error',
+                duration: 3 * 1000
+            })
 
-      if (res.status === 2) {
-        MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('FedLogOut').then(() => {
-            location.reload()
-          })
+            if (res.status === 2) {
+                MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
+                    confirmButtonText: '重新登录',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    store.dispatch('FedLogOut').then(() => {
+                        location.reload()
+                    })
+                })
+            }
+            return Promise.reject('error')
+        } else {
+            return response.data
+        }
+    },
+    error => {
+        Message({
+            message: error.message,
+            type: 'error',
+            duration: 3 * 1000
         })
-      }
-      return Promise.reject('error')
-    } else {
-      return response.data
+        return Promise.reject(error)
     }
-  },
-  error => {
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 3 * 1000
-    })
-    return Promise.reject(error)
-  }
 )
 
 export default service
